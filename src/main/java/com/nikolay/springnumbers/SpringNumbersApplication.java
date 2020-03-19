c4fac41361d5c699e21c1e3f862783de894b6c17
master-kot <master-kot@mail.ru>
2020-03-19 22:07:51 +0300

    Initial commit

---
+++ src/main/java/com/nikolay/springnumbers/SpringNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.nikolay.springnumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNumbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikolay/springnumbers/SpringNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolay.springnumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
