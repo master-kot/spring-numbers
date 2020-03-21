dd643b406ed47fc4cf11bcde6a09f52c5d188371
master-kot <master-kot@mail.ru>
2020-03-21 13:43:29 +0300

    Alpha version of web application has been released

--- src/main/java/com/nikolay/springnumbers/SpringNumbersApplication.java
+++ src/main/java/com/nikolay/springnumbers/SpringNumbersApplication.java
@@ -3,6 +3,13 @@ package com.nikolay.springnumbers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Класс запуска web-приложения на основе Spring Boot.
+ * Web-приложение создано по классической архитектуре Spring Boot,
+ * а именно работа с данными осуществляется последовательно через цепочку:
+ * Контроллер -> Сервис -> Репозиторий -> Сущность -> База данных,
+ * по условиям задания сохранение в БД не требовалось
+ */
 @SpringBootApplication
 public class SpringNumbersApplication {
 	public static void main(String[] args) {
